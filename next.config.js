6fd4d0683a4dc6c7cd30b6e1c9746dcbf0f95a3b
tian_2200@163.com <27998561@qq.com>
2025-06-10 10:43:29 +0800

    v1.52

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000', '*.pages.dev'],
+    },
+  },
+};
 
 if (process.env.NODE_ENV === 'development') {
   await setupDevPlatform();
