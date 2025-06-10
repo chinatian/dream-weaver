8dc3502c49aa8581fa2b77c00932df45249f1651
tian_2200@163.com <27998561@qq.com>
2025-06-10 10:07:05 +0800

    v1.8

--- next.config.js
+++ next.config.js
@@ -3,9 +3,19 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  // Configure for edge compatibility
   experimental: {
-    runtime: undefined,
-  }
+    webpackBuildWorker: false,
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        "async_hooks": false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig; 
